827174797f839faba1583325a29df43cbe88464a
rubakhan9430 <rubakhan9430@gmail.com>
2019-11-06 17:48:20 -0600

    distance

---
+++ Week2/distance.py
@@ -0,0 +1,8 @@
+speed = 60
+distance1 = speed * 5
+distance2 = speed * 8
+distance3 = speed * 12
+
+print('Distance in 5 hours:', distance1)
+print('Distance in 8 hours:', distance2)
+print('Distance in 12 hours:', distance3)
