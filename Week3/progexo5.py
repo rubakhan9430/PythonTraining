ab00af3ca235fb60fd3b9ff5f31807978d16298b
rubakhan9430 <rubakhan9430@gmail.com>
2019-11-11 19:07:18 -0600

    save

--- Week3/progexo5.py
+++ Week3/progexo5.py
@@ -1,12 +1,20 @@
-def assessmentvalue (actual):
+def taxproperty(actual):
     assessment = actual * 0.6
-    return (assessment)
-def propertytax(a):
-    tax = a * 0.0064
-    return (tax)
-actual = float(input('Property value: $'))
-b = assessmentvalue(actual)
-print('Assessment value: $',b)
-c = propertytax(b)
-print('Property Tax: $',c)
+    tax = assessment * 0.0064
+    return assessment,tax
 
+# def assessmentvalue (actual):
+#     assessment = actual * 0.6
+#     return assessment
+# def propertytax(a):
+#     tax = a * 0.0064
+#     return tax
+actual = float(input('Property value: $'))
+# b = assessmentvalue(actual)
+# print('Assessment value: $',b)
+# c = propertytax(b)
+# print('Property Tax: $',c)
+
+d,e = taxproperty(actual)
+print('Assessment: $',d)
+print('Property Tax: $',e)
