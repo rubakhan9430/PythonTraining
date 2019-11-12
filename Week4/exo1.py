2ea6d8f85fbe1bec226f2aecf9279b051b56eeb7
rubakhan9430 <rubakhan9430@gmail.com>
2019-11-11 20:27:40 -0600

    roman number

---
+++ Week4/exo1.py
@@ -0,0 +1,22 @@
+number = float(input('Enter a number between 1 and 10:'))
+if number >= 1 and number <= 10:
+    if number == 1:
+        print('Roman number: I')
+    elif number == 2:
+        print('Roman number: II')
+    elif number == 3:
+        print('Roman number: III')
+    elif number == 4:
+        print('Roman number: IV ')
+    elif number ==5:
+        print('Roman number: V')
+    elif number ==6:
+        print('Roman number: VI')
+    elif number ==7:
+        print('Roman number: VII')
+    elif number ==8:
+        print('Roman number: IX')
+    elif number ==10:
+        print('Roman number: X')
+else:
+    print('Number is not in range')
