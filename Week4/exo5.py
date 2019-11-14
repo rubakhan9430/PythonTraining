e1068231cd29e2d0bead2f5fc6178e0d1717db81
rubakhan9430 <rubakhan9430@gmail.com>
2019-11-14 14:26:56 -0600

    Color Mixer

---
+++ Week4/exo5.py
@@ -0,0 +1,10 @@
+primary1 = input('Enter Primary color 1:')
+primary2 = input('Enter Primary color 2:')
+if primary1 == 'red' and primary2 == 'blue':
+    print('Purple')
+elif primary1 == 'red' and primary2 == 'yellow':
+        print('Orange')
+elif primary1 == 'blue' and primary2 == 'yellow':
+        print('Green')
+else:
+    print('Error')
