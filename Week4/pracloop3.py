1ac4c0bd2a3a20ba646fe99901d022c10f4bf651
rubakhan9430 <rubakhan9430@gmail.com>
2019-11-18 21:18:12 -0600

    Titac

---
+++ Week4/pracloop3.py
@@ -0,0 +1,14 @@
+#write a program that runs a loop from 1 to 100
+#display tic if a number is divisible by 5
+#display tac if number divisible by 3
+#display titac if a number divisible by 3 and 5
+
+for n in range(1,101):
+    if n % 3 == 0 and n % 5 == 0:
+        print('titac')
+    elif n % 5 == 0:
+        print('tic')
+    elif n % 3 == 0:
+        print('tac')
+    
+    
