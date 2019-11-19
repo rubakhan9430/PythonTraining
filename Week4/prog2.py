8dd2d27fd4b9cc9f7d84c9c3a0494e0572ffa040
rubakhan9430 <rubakhan9430@gmail.com>
2019-11-18 21:02:05 -0600

    For calories loop

--- Week4/prog2.py
+++ Week4/prog2.py
@@ -1,5 +1,14 @@
-cal = 3.9
-L = [10,15,20,25,30]
-for n in L:
-    cal = n * 3.9
-    print('Calories:', cal)
+# cal = 3.9
+# L = [10,15,20,25,30]
+# for n in L:
+#     cal = n * 3.9
+#     print('Calories:', cal)
+
+
+# for x in range(10,35,5):
+#     print('You have burned:' + str(x*3.9) + 'calories during' + str(x) + 'minutes')
+x = 10
+while x < 40:
+    cal = x * 3.9
+    print('Calories',cal)
+    x = x + 5
